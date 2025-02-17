8eced5937533925f1fc90e0967f1eebff0a6ade2
dmytro-mewe <dmytro@mewe.com>
2025-02-17 22:02:15 +0100

    Refactor permissions

--- lib/modules/map/bloc/map_state.dart
+++ lib/modules/map/bloc/map_state.dart
@@ -4,12 +4,14 @@ part of 'map_bloc.dart';
 class MapState extends Equatable {
   const MapState({
     required this.mapInitialized,
+    this.showPermissionsRationale = false,
     this.selectedUser,
   });
 
   final bool mapInitialized;
+  final bool showPermissionsRationale;
   final UserPosition? selectedUser;
 
   @override
-  List<Object?> get props => [mapInitialized, selectedUser];
+  List<Object?> get props => [mapInitialized, showPermissionsRationale, selectedUser];
 }
